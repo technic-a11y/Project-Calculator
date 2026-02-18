75c621113be806ed1e950816a306c4e1ed4f14ce
technic-a11y <f_sohaib@yahoo.com>
2026-02-18 03:20:57 +0000

    Added some styles to the calculator and added a decimal point button. Also added CE and C buttons with different styles. Enabled keyborad input for the calculator. Deleted junk.txt file.

--- Pseudocode.md
+++ Pseudocode.md
@@ -1,4 +1,8 @@
 Add functions those simply add, subtract, multiply and divide.
 Create three variables for a number, an operator and another number.
-Create an operator function that evaluates the variables.
+Create an operator function that evaluates the above functions.
 Create buttons for every button in calculator including = operator in HTML.
+Add event listeners for the buttons from 0 to 9 and use array of operators to input operators.
+Create a backspace and clear buttons.
+Enable keyboard functionality by adding key events.
+
--- calculator.js
+++ calculator.js
@@ -44,7 +44,7 @@ button.forEach (button => {
     button.addEventListener ('click', () => {
      const value = button.textContent;
     
-     if (!isNaN(value)) {
+     if (value >= '0' && value <= '9') {
         if (op === '') {
             num1 += value;
             display.textContent = num1;
@@ -68,8 +68,63 @@ button.forEach (button => {
         num2 = '';
         op = '';
         display.textContent = '0';
+    } else if ( value === '.') {
+        if (op === '') {
+            if (num1.includes('.')) return;
+            num1 += value;
+            display.textContent = num1;
+        }
+    } else if (value === 'BackSpace') {
+        if (op === '') {
+            num1 = num1.slice(0, -1);
+            display.textContent = num1 || '0';
+        }
+        else {
+            num2 = num2.slice(0, -1);
+            display.textContent = num2 || '0';
+        }
     }
-
+    button.addEventListener('keyup', (event) => {
+        
+        if (event.key === 'Enter') {
+            operate();
+        }
+            else if (event.key === 'Backspace') {
+                if (op === '') {
+                    num1 = num1.slice(0, -1);
+                    display.textContent = num1 || '0';                
+                }
+                else { 
+                    num2 = num2.slice(0, -1);
+                    display.textContent = num2 || '0';
+                }
+            }
+            else if (event.key >= '0' && event.key <= '9') {
+                if (op === '') {
+                    num1 += event.key;
+                    display.textContent = num1;
+                } else {
+                    num2 += event.key;
+                    display.textContent = num2;
+                }
+            } else if (['+','-','*','/'].includes(event.key)) {
+                if (num1 === '') return;
+                op = event.key;
+                display.textContent = op;
+            } else if (event.key === '.') {
+                if (op === '') {
+                    num1 += event.key;
+                    display.textContent = num1;
+                }
+                else {
+                    num2 += event.key;
+                    display.textContent = num2;
+                }
+            }
+            event.preventDefault();
+        event.stopPropagation();
+    })
+    
     })
 });
 // figure out how to get the calculations to work on with the equals button.
